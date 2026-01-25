2795602b5eaf7ca93d74a78f52bc6948466b912f
Herasi Silva <herasidesweb@gmail.com>
2026-01-25 19:49:08 -0400

    Update categories.php

--- views/admin/categories.php
+++ views/admin/categories.php
@@ -1,8 +1,9 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-?>
-<?php
+
+require_once __DIR__ . '/../../config/config.php';
+
 $page_title = "Gestión de Categorías";
 require_once __DIR__ . '/../layouts/header.php';
 
